0021e414502cef31313b1466191c4f5f92dc0267
Ritika-kushwaha <ritikakushwaha62@gmail.com>
2026-07-04 14:27:03 +0530

    Time: 18 ms (38.83%) | Memory: 107.8 MB (8.81%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,7 +2,6 @@ import java.util.HashSet;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Create a set to store the unique numbers we have seen so far
         HashSet<Integer> seen = new HashSet<>();
         
         for (int num : nums) {
