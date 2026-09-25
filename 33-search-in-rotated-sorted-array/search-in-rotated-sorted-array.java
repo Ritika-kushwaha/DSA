fafe0eec7aa6c0389460b658f8deb4f824871dd2
Ritika-kushwaha <ritikakushwaha62@gmail.com>
2026-09-25 17:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (28.60%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -7,21 +7,21 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[left]<=nums[mid]){
-                if(target>=nums[left] && target<nums[mid]){
+            if(nums[mid]>=nums[left]){
+                if(nums[mid]>target && nums[left]<=target){
                     right=mid-1;
-                }
-                else{
+                }else{
                     left=mid+1;
                 }
-            }else{
-                if(target>nums[mid]&& target<=nums[right]){
+            }
+            else{
+                if(target<=nums[right] && target>nums[mid]){
                     left=mid+1;
+
                 }else{
                     right=mid-1;
                 }
             }
-            
         }
         return -1;
     }
